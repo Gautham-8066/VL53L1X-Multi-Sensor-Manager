72e48885daefd889cd53315b4057a43e0fb1f07e
Gautham Chenoth Praveen <146088305+Gautham-8066@users.noreply.github.com>
2025-11-27 18:53:57 +0530

    Add UNIT_TEST preprocessor directive
    Added a preprocessor directive for unit testing.

---
+++ src/VL53L1X_Manager.h
@@ -0,0 +1,26 @@
+#ifndef VL53L1X_MANAGER_H
+#define VL53L1X_MANAGER_H
+
+#define UNIT_TEST 
+
+#include <Arduino.h>
+
+#ifdef UNIT_TEST
+  #include "MockVL53L1X.h"
+#else
+  #include <VL53L1X.h>     
+#endif
+
+class VL53L1X_Manager {
+  public:
+    VL53L1X_Manager(VL53L1X *sensors, uint8_t *xshutPins, uint8_t count);
+    bool begin();
+
+  private:
+    VL53L1X *_sensors;
+    uint8_t *_xshutPins;
+    uint8_t _count;
+    const uint8_t START_ADDR = 0x30;
+};
+
+#endif
